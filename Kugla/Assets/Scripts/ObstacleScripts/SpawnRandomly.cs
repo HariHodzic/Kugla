c5795eabbb3bab68722a9c7eb458be9de020091b
Haris Hodžić <hari.hodzic98@gmail.com>
2020-05-31 22:47:02 +0200

    obstacles position and size update

--- Kugla/Assets/Scripts/ObstacleScripts/SpawnRandomly.cs
+++ Kugla/Assets/Scripts/ObstacleScripts/SpawnRandomly.cs
@@ -21,7 +21,7 @@ public class SpawnRandomly : MonoBehaviour
     private void SpawnObstacleFromPool()
     {
         lastTime = Time.time;
-        Vector3 position = new Vector3(kugla.transform.position.x+20,1,kugla.transform.position.z);
+        Vector3 position = new Vector3(Random.Range(kugla.transform.position.x+20, kugla.transform.position.x + 50), 0.5f, Random.Range(1.3f,4.8f));
         var obstacle = BasicPool.Instance.GetFromPool();
         obstacle.transform.position = position;
     }
